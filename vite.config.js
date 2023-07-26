c9078f1d32bdd8004b1d953a334c2953515d031b
Thomas Rich <tnrich@teselagen.com>
2023-07-26 16:32:08 -0700

    updating and simplifying deps

--- vite.config.js
+++ vite.config.js
@@ -10,32 +10,6 @@ export default {
       },
     },
   },
-  resolve: {
-    alias: {
-      "@teselagen/ove": path.resolve(
-        __dirname,
-        "./node_modules/@teselagen/ove"
-      ),
-      "@teselagen/ui": path.resolve(__dirname, "./node_modules/@teselagen/ui"),
-      "@teselagen/range-utils": path.join(
-        __dirname,
-        "node_modules/@teselagen/range-utils"
-      ),
-      "@teselagen/file-utils": path.join(
-        __dirname,
-        "node_modules/@teselagen/file-utils"
-      ),
-      dayjs: path.join(__dirname, "node_modules/dayjs"),
-      "@teselagen/sequence-utils": path.join(
-        __dirname,
-        "node_modules/@teselagen/sequence-utils"
-      ),
-      "@teselagen/bio-parsers": path.join(
-        __dirname,
-        "node_modules/@teselagen/bio-parsers"
-      ),
-    },
-  },
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
